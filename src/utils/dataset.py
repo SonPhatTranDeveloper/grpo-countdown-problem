fca9816eed58b030bdbc1e2b15406a5e37730415
SonPhatTranDeveloper <phat.tran.developer@gmail.com>
2025-09-24 11:24:27 +0700

    Update mapping function

--- src/utils/dataset.py
+++ src/utils/dataset.py
@@ -1,6 +1,21 @@
+from collections.abc import Callable
+
 from datasets import Dataset, load_dataset
 
-SYSTEM_PROMPT = """
+
+def map_problem_description_to_conversation(
+    row: dict[str, any],
+) -> list[dict[str, any]]:
+    """
+    Map a problem description to a conversation.
+
+    Args:
+        row: The row
+
+    Returns:
+        list[dict[str, any]]: The conversation
+    """
+    system_prompt = """
 You are a helpful AI Assistant that follows user's instructions.
 - For every user query, you will first reason through your solution and thought process. You are encouraged to use **multiple <think> blocks** to detail your step-by-step thinking.
 - After your reasoning, provide your final response to the user. This response must be contained within <answer> tags.
@@ -22,36 +37,23 @@ You are a helpful AI Assistant that follows user's instructions.
 
 There should ONLY be ONE <answer> block.
 """
-
-
-def map_proble_description_to_conversation(
-    row: dict[str, any],
-) -> list[dict[str, any]]:
-    """
-    Map a problem description to a conversation.
-
-    Args:
-        row: The row
-
-    Returns:
-        list[dict[str, any]]: The conversation
-    """
     return [
-        {"role": "system", "content": SYSTEM_PROMPT},
+        {"role": "system", "content": system_prompt},
         {"role": "user", "content": row["problem_description"]},
     ]
 
 
-def load_csv_dataset(file_path: str) -> Dataset:
+def load_csv_dataset(file_path: str, mapping_function: Callable) -> Dataset:
     """
     Load a CSV dataset.
 
     Args:
         file_path: Path to the CSV file
+        mapping_function: Function to map the dataset
 
     Returns:
         Dataset: The loaded dataset
     """
     dataset = load_dataset("csv", data_files=file_path)
-    dataset = dataset.map(map_proble_description_to_conversation)
+    dataset = dataset.map(mapping_function)
     return dataset
